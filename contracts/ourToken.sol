d849c7d3826b7cc8ebafeaacca69860afe1c85f4
narendra-sajwani <niks.imagination@gmail.com>
2022-09-22 12:19:22 +0530

    add constructor

--- contracts/ourToken.sol
+++ contracts/ourToken.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OurToken is ERC20 {}
+contract OurToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
